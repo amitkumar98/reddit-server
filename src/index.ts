0016cc8ca712f03c512ff6f18a8b04d0b187bb37
Amit-x360 <amit.amit.kumar761@gmail.com>
2021-12-26 16:09:19 +0530

    user register and login with basic validations

--- src/index.ts
+++ src/index.ts
@@ -6,6 +6,7 @@ import express from "express";
 import { ApolloServer } from "apollo-server-express";
 import { buildSchema } from "type-graphql";
 import { PostResolver } from "./resolvers/post";
+import { UserResolver } from "./resolvers/user";
 
 const main = async () => {  
     // connect to database
@@ -17,7 +18,7 @@ const main = async () => {
 
     const apolloServer = new ApolloServer({
         schema: await buildSchema({
-            resolvers: [PostResolver],
+            resolvers: [UserResolver, PostResolver],
             validate: false
         }),
         context: () => ({ em: orm.em })
