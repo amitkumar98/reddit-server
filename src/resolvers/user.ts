d5e876fd73e504fb377d9e546e55f0352743cec7
Amit-x360 <amit.amit.kumar761@gmail.com>
2022-01-08 12:50:07 +0530

    Added logout mutation

--- src/resolvers/user.ts
+++ src/resolvers/user.ts
@@ -11,6 +11,7 @@ import {
   Resolver,
 } from "type-graphql";
 import argon2 from "argon2";
+import { COOKIE_NAME } from "../constants";
 
 @InputType()
 class UserNamePasswordInput {
@@ -131,5 +132,20 @@ export class UserResolver {
     req.session.userId = user.id;
 
     return { user };
+  }
+
+  @Mutation(() => Boolean)
+  logout(@Ctx() { req, res }: MyContext) {
+    return new Promise((resolve) =>
+      req.session.destroy((err) => {
+        res.clearCookie(COOKIE_NAME);
+        if (err) {
+          console.log(err);
+          resolve(false);
+          return;
+        }
+        resolve(true);
+      })
+    );
   }
 }
